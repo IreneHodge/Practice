4235024615b12a16ff454fe28eec6bca4f8d45b0
Irene Hodge <irenehodge90@gmail.com>
2021-07-07 14:20:54 -0700

    updated css to add background images

---
+++ Drum_Kit/index.js
@@ -0,0 +1,15 @@
+
+for(var i = 0; i <document.querySelectorAll(".drum").length; i++){
+
+    // document.querySelectorAll("button")[i].addEventListener("click", function(){
+    //     alert("I got clicked!")
+    // })
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+
+    })
+    
+}
+
