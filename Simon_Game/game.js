02c7a9a0f9ba8e13dcbf3f30018157a8845f85fc
Irene Hodge <irenehodge90@gmail.com>
2021-07-09 16:09:20 -0700

    added game restart logic

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -63,8 +63,8 @@ function checkAnswer(currentLevel) {
             }, 1000)
         }
     } else {
-        var audio = new Audio("sounds/wrong.mp3");
-        audio.play();
+
+       playSound('wrong');
 
         $("body").addClass("game-over");
        
@@ -72,11 +72,17 @@ function checkAnswer(currentLevel) {
             $("body").removeClass("game-over")
         }, 200)
 
-        $("h1").text("Game Over, Press Any Key to Restart")
+        $("#level-title").text("Game Over, Press Any Key to Restart")
+        
+       startOver();
+
 
     }
 
-  
-
 }
 
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStart = false;
+}
