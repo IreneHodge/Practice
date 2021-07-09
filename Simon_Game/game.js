76d2f21af5194a40e50d6c821d7dcd1a057428d7
Irene Hodge <irenehodge90@gmail.com>
2021-07-09 15:55:10 -0700

    added end of game logic - updated, heading, styles and sounds

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -63,7 +63,17 @@ function checkAnswer(currentLevel) {
             }, 1000)
         }
     } else {
-        console.log("wrong")
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+
+        $("body").addClass("game-over");
+       
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200)
+
+        $("h1").text("Game Over, Press Any Key to Restart")
+
     }
 
   
