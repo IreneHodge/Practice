9e1d52b38af2da19b708cf3c38d5e21b8e7f0218
Irene Hodge <irenehodge90@gmail.com>
2021-07-09 08:39:06 -0700

    refactored sound so could be used by both computer and user // created user button click

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -1,15 +1,26 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
-    
+
     var randomNumber = Math.floor((Math.random() * 4));
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+})
 
