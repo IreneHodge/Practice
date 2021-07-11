7c9ce202639c9f488024c19dbf8e6bb8bfb902ef
Irene Hodge <irenehodge90@gmail.com>
2021-07-11 08:13:13 -0700

    basic express server

---
+++ my-express-server/server.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact me at irenehodge90@gmail.com");
+});
+
+app.get("/about", (req, res) => {
+  res.send("I'm Irene! I am a fullstack software engineer. :)");
+});
+app.listen(port, () => {
+  console.log("You are connected");
+});
