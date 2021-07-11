7f3b58cbf364eb625058f8d7c9dc7425e2036393
Irene Hodge <irenehodge90@gmail.com>
2021-07-11 10:58:49 -0700

    calculator - express playing with paths

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+// app.use(bodyParser.json())
+
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/", (req, res) => {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = (weight/(Math.pow(height, 2))) * 703
+    res.send("Your BMI is " + bmi)
 })
 
 app.listen(port, () => {
